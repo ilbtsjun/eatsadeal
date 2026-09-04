1631ba8254726fb2039dbbac50090e5aa955b7e5
전태성 <ilbtsjun@gmail.com>
2026-09-04 10:32:42 +0900

    닉네임으로도 로그인 가능하게 변경, user엔티티의 생성제약조건 추가

--- backend/src/main/java/com/backend/user/dto/LoginRequest.java
+++ backend/src/main/java/com/backend/user/dto/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.backend.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "로그인 요청")
